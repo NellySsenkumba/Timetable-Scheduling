abea096c420f9f2042fa9a723e6b7277a6006051
Nelly Ssenkumba <ssenkumbanelson.sn@gmail.com>
2023-09-29 20:34:46 +0300

    initial commit

---
+++ src/main/java/org/school/timetableschedulingsystem/TimetableSchedulingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.school.timetableschedulingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableSchedulingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimetableSchedulingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/school/timetableschedulingsystem/TimetableSchedulingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.school.timetableschedulingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableSchedulingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
